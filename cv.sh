371ab27586466b30a178458066085dc8bccb6955
Luiz Barbosa <l.barbosa@withplum.com>
2026-04-16 22:57:37 +0100

    Update CV

---
+++ cv.sh
@@ -0,0 +1,4 @@
+swift run CVGenerator --templates ./Templates --input resume-iOS-staff.json --pdf
+swift run CVGenerator --templates ./Templates --input resume-iOS-senior.json --pdf
+rm resume-iOS-staff.html
+rm resume-iOS-senior.html
